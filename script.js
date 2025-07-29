85fedc0b443ba113c6090453bba71b71f14789e0
tomasneuparth <tasnarq@gmail.com>
2025-07-29 19:12:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const images = document.querySelectorAll(".carousel-image");
+let current = 0;
+
+setInterval(() => {
+  images[current].classList.add("hidden");
+  current = (current + 1) % images.length;
+  images[current].classList.remove("hidden");
+}, 3000); // change image every 3 seconds
