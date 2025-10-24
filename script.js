48be19db8cb65123f99ab5d29e8a25d49763e484
tomasneuparth <tasnarq@gmail.com>
2025-10-24 16:50:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const slides = document.querySelectorAll('.carousel img');
-let idx = 0;
-setInterval(() => {
-  slides[idx].classList.remove('active');
-  idx = (idx + 1) % slides.length;
-  slides[idx].classList.add('active');
-}, 4000);
